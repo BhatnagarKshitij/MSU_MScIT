277c1b357d5a15bfa7dc6ceb7142e99ac9e76436
BhatnagarKshitij <kshitijbhatnagar123@gmail.com>
2019-10-11 22:05:31 +0000

    Completed till ex19

---
+++ IST/Ex1.sh
@@ -0,0 +1,79 @@
+#Question 1
+mkdir MSc_1
+#Question 2
+cd MSc_1
+#Question 3
+pwd
+#Question 4
+mkdir Shell_Prog
+#Question 5
+cd Shell_Prog
+#Question 6
+touch student_list
+echo "Kshitij
+Harsh
+Parth
+Juned
+Soyab
+Joy
+Harshil
+Vishal
+Aka Romaricq Junior
+" >> student_list
+#Question 7
+echo "Word COunt `wc -cwl student_list`"
+#Question 8
+cp student_list student_list_backup
+#Question 9
+cp student_list /student_list
+#Question 10
+cal
+#Question 11
+cal JAN 2004
+#Question 12
+date +%D%A%T
+#Question 13
+echo "Todays date is `+%d/%m/%y`"
+#Question 14
+echo "Todays Day is `date +%a`"
+#Question 15
+echo "Today day is `date +%d`"
+#Question 16
+echo "Current Time is `date +%T`"
+#Question 17
+echo "Eyes
+Nose
+Teeth
+Mouth
+" >> Biology
+
+echo "Zinc
+Cobalt
+Mercury
+Copper
+" >> Chemistry
+
+echo "Light
+Temperature
+force
+acceleration
+" >> Physics
+
+echo "Addition
+Subtration
+Multiplication
+Division
+" >> Mathematics
+#Question 18
+cat Mathematics Physics Chemistry Biology > Science
+#Question 19
+echo "This size in character of File BIOLOGY are `wc -c Biology`"
+echo "This size in character of File CHEMISTRY are `wc -c Chemistry`"
+echo "This size in character of File PHYSICS are `wc -c Physics`"
+echo "This size in character of File MATHEMATICS are `wc -c Mathematics`"
+echo "The Total number of Lines in all four files are `wc -l Science`"
+echo "The Total number of Words in all four files are `wc -w Science`"
+echo "The Total number of Character in all four files are `wc -c Science`"
+#Question 20
+
+
