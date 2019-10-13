f99bb6c4586f2d379a46492f663362a76abdb8e7
root <root@localhost.localdomain>
2019-10-13 01:02:41 +0000

    COMPLETED EX1

--- IST/Ex1/Ex1.sh
+++ IST/Ex1/Ex1.sh
@@ -75,5 +75,42 @@ echo "The Total number of Lines in all four files are `wc -l Science`"
 echo "The Total number of Words in all four files are `wc -w Science`"
 echo "The Total number of Character in all four files are `wc -c Science`"
 #Question 20
-
-
+wc -cwl Science > Summery_input
+#Question 21
+pwd
+#Question 22
+uname -o
+#Question 23
+mv Summery_input final_summery
+#Question 24
+cp Biology Chemistry Physics Mathematics /root
+#Question 25
+touch prog1.txt prog2.txt prog3.txt prog4.txt prog5.doc prog6.doc data1.doc data2.doc data3.doc data4.doc record1 record2 record3 record4
+#Question 26
+ls prog*
+#Question 27
+ls data*
+#Question 28
+ls *txt
+#Question 29
+ls *doc
+#Question 30
+ls *2
+#Question 31
+ls *1
+#Question 32
+ls *3
+#Question 33
+ls *{1,2,3}
+#Question 34
+ls *{1,2,3,4}
+#Question 35
+ls *{doc,txt}
+#Question 36
+ls *[^txt]
+ls *[!t][!x][!t]
+#Question 37
+ps
+#Question 38
+ls *[!0-9]
+ls *[^0-9]
