2c3c49f45a2d8794e950a08574f50368fc47fd42
BhatnagarKshitij <kshitijbhatnagar123@gmail.com>
2019-10-14 23:14:18 +0530

    Completed Question 24

---
+++ JAVA/Question24/src/question24/Animal.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package question24;
+
+/**
+ *
+ * @author Kshitij
+ */
+public class Animal {
+    static int no_of_leg=4;
+    String animalName;
+    public static class AquaticAnimals{
+        public void display(){
+            System.out.println("Fish has "+no_of_leg+" Legs");
+        }
+}
+    
+}
